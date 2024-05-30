22c9289294de0fd66b2e918204044994e485b4fe
Oluwasuyi-Timilehin <146850350+Oluwasuyi-Timilehin@users.noreply.github.com>
2024-05-30 23:19:43 +0100

    made changes

--- intro/src/Components/Signup.jsx
+++ intro/src/Components/Signup.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Signup = () => {
   return (
     <>
       <section className="lg:flex lg:w-full lg:h-screen">
-        <div className="lg:h-screen lg:w-1/2 bg-primary">hi my name is </div>
+        <div className="hidden lg:h-screen lg:w-1/2 lg:flex lg:justify-center lg:items-center lg:px-10 bg-primary">
+          <img src="./src/Images/logoresize.jpg" className="object-cover rounded-md" />
+        </div>
         <div className="mx-auto w-1/2 h-screen flex flex-col items-center justify-center space-y-5">
           <p className="font-semibold lg:text-4xl">Sign Up</p>
           <form className="flex w-full flex-col justify-center space-y-5 lg:border lg:border-grey lg:shadow-xl lg:px-8 lg:rounded-xl lg:py-7 lg:w-96">
@@ -27,6 +30,12 @@ const Signup = () => {
             <button className="font-bold bg-primary text-white py-2 rounded-lg ">
               Sign Up
             </button>
+            <p className="text-center text-primary">
+              Already have an account?{" "}
+              <Link to="" className="underline text-red-600">
+                log in
+              </Link>
+            </p>
           </form>
         </div>
       </section>
