d2a8da79875e77372e03623607e565fb3c241ee7
Oluwasuyi-Timilehin <146850350+Oluwasuyi-Timilehin@users.noreply.github.com>
2025-01-15 00:18:37 +0100

    updated

--- intro/src/Router/Routing.jsx
+++ intro/src/Router/Routing.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import {BrowserRouter, Routes, Route} from "react-router-dom"
-import Homepage from '../Pages/Homepage'
-import MovieDetails from '../Components/MovieDetails'
-import Topratedpage from '../Pages/Topratedpage'
-import Blog from '../Pages/Blog'
-import Signup from '../Components/Signup'
-import Login from '../Components/Login'
-import Contact from '../Components/Contact'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Homepage from "../Pages/Homepage";
+import MovieDetails from "../Components/MovieDetails";
+import Topratedpage from "../Pages/Topratedpage";
+import Blog from "../Pages/Blog";
+import Signup from "../Components/Signup";
+import Login from "../Components/Login";
+import Contact from "../Components/Contact";
 
 const Routing = () => {
   return (
@@ -15,15 +15,15 @@ const Routing = () => {
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/MovieDetails/:id" element={<MovieDetails />} />
-          <Route path='/blog' element={<Blog />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/login' element={<Login />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/topratedpage" element={<Topratedpage />} />
-          <Route path="/contact" element={<Contact/>} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </BrowserRouter>
     </>
   );
-}
+};
 
-export default Routing
+export default Routing;
